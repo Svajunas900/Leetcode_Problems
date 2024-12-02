05f217eb2e6f40fe8ed2a0f26308b89c141f95a3
Svajunas Koncius <svajunas.koncius@mera.com>
2024-12-02 13:33:41 +0200

    Refactor problem_1455 using startswith method

--- problem_1455.py
+++ problem_1455.py
@@ -1,12 +1,8 @@
 def isPrefixOfWord(sentence, searchWord):
     words = sentence.split()
-    searchLength = len(searchWord)
     for index, word in enumerate(words):
-        lengthWord = len(word)
-        if searchLength > lengthWord:
-            continue
-        if word[:searchLength] == searchWord:
-            return index + 1
+        if word.startswith(searchWord):
+            return index+1
     return -1
 
 
