4d542546f2e82901b3b831ade1323bdd65c68288
Svajunas Koncius <svajunas.koncius@mera.com>
2024-12-02 13:40:13 +0200

    Refactor problem_1455 using list comprehension

--- problem_1455.py
+++ problem_1455.py
@@ -1,9 +1,10 @@
 def isPrefixOfWord(sentence, searchWord):
     words = sentence.split()
-    for index, word in enumerate(words):
-        if word.startswith(searchWord):
-            return index+1
-    return -1
+    result = [index+1 for index, word in enumerate(words) if word.startswith(searchWord)]
+    if result:
+        return result[0]
+    else:
+        return -1
 
 
 print(isPrefixOfWord("i love eating burger", "burg"))
