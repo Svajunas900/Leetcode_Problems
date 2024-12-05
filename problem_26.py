200868dbd784a33cdc4a6deccdc788c0182fc283
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-05 11:05:43 +0200

    Add alternative solution to problem_26

--- problem_26.py
+++ problem_26.py
@@ -12,9 +12,12 @@ Return k.
 """
 
 def removeDuplicates(nums):
-    i = 1
-    for j in range(1,len(nums)):
-        if nums[j] != nums[i-1]:
-            nums[i] = nums[j]
-            i += 1
-    return i
+    num = set(nums)
+    print(num)
+    return len(num)
+
+    # With set
+    # for i, x in enumerate(sorted(set(nums))):
+    #     nums[i] = x
+    # return len(sorted(set(nums)))
+print(removeDuplicates([1,1,2]))
