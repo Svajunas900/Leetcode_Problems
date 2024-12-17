943d65d194ac2d62d26071868b0da56dfd5e4c28
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-17 10:30:19 +0200

    Add Leetcode problem_118

---
+++ problem_118.py
@@ -0,0 +1,23 @@
+"""118. Pascal's Triangle
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+"""
+
+
+def generate(numRows):
+    if numRows == 0:
+        return []
+    if numRows == 1:
+        return [[1]]
+    
+    prevRows = generate(numRows-1)
+    newRow = [1] * numRows
+
+    for i in range(1, numRows-1):
+        newRow[i] = prevRows[-1][i-1] + prevRows[-1][i]
+    
+    prevRows.append(newRow)
+    return prevRows
+
