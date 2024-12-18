7537d35aab257d45f2abb07caa446227c8b8d93c
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-18 10:55:12 +0200

    Add Leetcode problem_136

---
+++ problem_136.py
@@ -0,0 +1,15 @@
+"""136. Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+"""
+
+
+def singleNumber(nums):
+    uniqNum = 0
+    for idx in nums:
+        uniqNum ^= idx
+    return uniqNum; 
+
+print(singleNumber([4,1,2,1,2]))
