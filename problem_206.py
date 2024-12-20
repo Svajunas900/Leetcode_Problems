e9a7b14ecaf8ebb16ebb4e790ab44cbdc2e501e0
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-20 10:20:20 +0200

    Add Leetcode problem_206

---
+++ problem_206.py
@@ -0,0 +1,20 @@
+"""206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+"""
+
+
+
+def reverseList(head):
+    if not head:
+        return 
+    previous = None
+    curr = head
+    ahead = head.next
+    while ahead:
+        curr.next = previous
+        previous = curr
+        curr = ahead
+        ahead = ahead.next
+    curr.next = previous
+    return curr
