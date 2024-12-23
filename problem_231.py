8da1add2c66322a6ea170831eb431e5f688eff16
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-23 10:09:16 +0200

    Add Leetcode problem_231

---
+++ problem_231.py
@@ -0,0 +1,16 @@
+"""231. Power of Two
+
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+"""
+
+def isPowerOfTwo(n):
+    if n == 2 or n == 1:
+        return True
+    s = 2
+    while s <= n:
+        if s == n:
+            return True
+        s *= 2
+    return False
