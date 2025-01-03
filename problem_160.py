99634064229fb3d9cafc7150fba421659177b650
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-03 10:04:23 +0200

    Add Leetcode problem_160

---
+++ problem_160.py
@@ -0,0 +1,14 @@
+"""160. Intersection of Two Linked Lists
+
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+If the two linked lists have no intersection at all, return null.
+
+"""
+
+
+def getIntersectionNode(headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    a, b = headA, headB
+    while (a != b):
+        a = headB if not a else a.next
+        b = headA if not b else b.next
+    return a
