340c75d8520ab303c4a7ea1f4298f73e1d8dfe65
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-07 09:28:53 +0200

    Add Leetcode problem_219

---
+++ problem_219.py
@@ -0,0 +1,14 @@
+"""219. Contains Duplicate II
+
+Given an integer array nums and an integer k, 
+return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+"""
+
+
+def containsNearbyDuplicate(nums:list, k: int) -> bool:
+  hset = {}
+  for idx in range(len(nums)):
+      if nums[idx] in hset and abs(idx - hset[nums[idx]]) <= k:
+          return True
+      hset[nums[idx]] = idx
+  return False
