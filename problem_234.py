77a02c7887d47b69a69b165824eedae56ed79e79
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-10 09:41:03 +0200

    Add leetcode problem_234

---
+++ problem_234.py
@@ -0,0 +1,15 @@
+"""234. Palindrome Linked List
+
+Given the head of a singly linked list, 
+return true if it is a palindrome or false otherwise.
+"""
+def isPalindrome(self, head) -> bool:
+        self.curr = head
+        return self.solve(head)
+    
+def solve(self, head):
+    if head is None:
+        return True
+    ans = self.solve(head.next) and self.curr.val == head.val
+    self.curr = self.curr.next
+    return ans
