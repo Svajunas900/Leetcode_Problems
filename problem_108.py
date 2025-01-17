15acb5b02a19a7325fba4e82992e0d6fb13828da
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-17 09:51:53 +0200

    Add Leetcode problem_108

---
+++ problem_108.py
@@ -0,0 +1,17 @@
+"""108. Convert Sorted Array to Binary Search Tree
+
+
+Given an integer array nums where the elements are sorted in ascending order, convert it to a 
+height-balanced binary search tree.
+"""
+
+def sortedArrayToBST(self, nums: list) -> Optional[TreeNode]:
+  total_nums = len(nums)
+  if not total_nums:
+      return None
+  
+  mid_node = total_nums // 2
+  return TreeNode(
+      nums[mid_node],
+      self.sortedArrayToBST(nums[:mid_node]), self.sortedArrayToBST(nums[mid_node+1:])
+  )
