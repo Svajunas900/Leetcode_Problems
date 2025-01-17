d747ffb36ae988ba9e3eebec16560676db0c38a2
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-17 10:13:04 +0200

    Add Leetcode problem_119

---
+++ problem_119.py
@@ -0,0 +1,15 @@
+"""119. Pascal's Triangle II
+
+
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+"""
+
+
+def getRow(self, rowIndex: int) -> List[int]:
+  row = [1]
+  for i in range(1, rowIndex + 1):
+      next_element = row[i - 1] * (rowIndex - i + 1) // i
+      row.append(next_element)
+  return row
