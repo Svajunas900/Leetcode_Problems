e1fb33ed6d9a7f878a1239a85f776a4520389932
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-20 13:49:14 +0200

    Add Time and space complexity without explanation to problem_226

--- problem_226.py
+++ problem_226.py
@@ -3,6 +3,11 @@
 Given the root of a binary tree, invert the tree, and return its root.
 """
 
+"""Time complexity Big O(n)
+
+Space complexity Big O(n)
+"""
+
 def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
   if not root:
       return root
