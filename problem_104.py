91e12a8b167829016f4f1bc6f4c4a29fbb8feacf
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-20 13:04:53 +0200

    Add Time and space complexity without explanation to problem_104

--- problem_104.py
+++ problem_104.py
@@ -5,7 +5,10 @@ Given the root of a binary tree, return its maximum depth.
 A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
 """
+"""Time complexity Big O(n)
 
+Space complexity Big O(n)
+"""
 def maxDepth(root: Optional[TreeNode]) -> int:
     if not root:
         return 0
