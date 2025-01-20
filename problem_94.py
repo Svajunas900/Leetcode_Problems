d60cc890fc2974b13e06905da724d95fa4a53d41
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-20 13:01:04 +0200

    Add Time and space complexity without explanation to problem_94

--- problem_94.py
+++ problem_94.py
@@ -9,24 +9,18 @@ class TreeNode:
       self.left = left
       self.right = right
 
+"""Time complexity Big O(n)
+
+Space complexity Big O(n)
+"""
 def inorderTraversal(root) -> list:
-  return traverse(root)
-
-def traverse(root):
-    if root is None:
-        return
-    if root.left:
-        traverse(root.left)
-        print(root.val)
-    if root.right:
-        traverse(root.right)
-        print(root.val)
-
-
-node1 = TreeNode(1)
-node2 = TreeNode(2)
-node3 = TreeNode(3)
-node1.left = node2
-node2.right = node3
-
-print(inorderTraversal(node1))
+  result = []
+  def inorder(root):
+      if not root:
+          return []
+      inorder(root.left)
+      result.append(root.val)
+      inorder(root.right)
+      return result
+  inorder(root)
+  return result
