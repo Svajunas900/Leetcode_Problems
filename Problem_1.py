de1cdef89710403125b11fd180e18803399bdc3b
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-20 11:42:17 +0200

    Add Time and space complexity explanation

--- Problem_1.py
+++ Problem_1.py
@@ -4,8 +4,17 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order."""
 
+"""Time complexity Big O(n)
+The number of iterations is proportional to length of nums
+To check if the target - n is in dictionary O(1)
+Inserting new entry to dictionary O(1)
+So the dominant term is the iteration through list O(n)
 
-def twoSum(nums: int, target:int) -> list:
+Space complexity O(n)
+Using dictionary to store n elements, where n is the number of elements in the
+input list nums
+"""
+def twoSum(nums: int, target:int) -> list: 
     a_dict = {}
     for i, n in enumerate(nums):
         if target - n in a_dict:
