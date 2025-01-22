6db84daf829f237a98caa26112f98165600ec682
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-22 10:17:44 +0200

    Add explanation to problem_31

--- problem_31.py
+++ problem_31.py
@@ -3,7 +3,8 @@
 
 A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
 
-For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+For example, for arr = [1,2,3], the following are all the permutations of arr: 
+[1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
 The next permutation of an array of integers is the next lexicographically greater permutation of its integer. 
 More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, 
 then the next permutation of that array is the permutation that follows it in the sorted container. 
@@ -11,10 +12,18 @@ If such arrangement is not possible, the array must be rearranged as the lowest 
 
 For example, the next permutation of arr = [1,2,3] is [1,3,2].
 Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
-While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] 
+does not have a lexicographical larger rearrangement.
 Given an array of integers nums, find the next permutation of nums.
 
 The replacement must be in place and use only constant extra memory.
+"""
+
+"""Time complexity Big O(n)
+reverse is O(n), while O(n) in worst cases, overall dominant operation O(n)
+
+Space complexity Big O(1)
+It modifies nums in-place
 """
 
 def nextPermutation(self, nums: List[int]) -> None:
