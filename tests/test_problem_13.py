8613a2a301ac1758abcfc02a45d1abf5ec1c3f88
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-14 15:47:10 +0200

    Refactored test_problem_13 to use Unittest instead of pytest

--- tests/test_problem_13.py
+++ tests/test_problem_13.py
@@ -1,4 +1,4 @@
-import pytest
+import unittest
 from problem_13 import romanToInt
 
 
@@ -15,12 +15,15 @@ test_data_failure = [(122, True),
              ]
 
 
-@pytest.mark.parametrize("nums, expected", test_data_success)
-def test_romanToInt_success(nums, expected):
-  assert romanToInt(nums) == expected
+class TestThreeSum(unittest.TestCase):
+  def test_success(self):
+    self.assertEqual(romanToInt("MCCIV"), 1204)
+    self.assertEqual(romanToInt("MCMX"), 1910)
+    self.assertEqual(romanToInt("MXCIV"), 1094)
 
+  def test_failure(self):
+    self.assertFalse(romanToInt("MCCIV") == 191)
+    self.assertFalse(romanToInt("MCMXCIV") == 1910)
+    self.assertFalse(romanToInt("MCMX") == 190)
+    self.assertFalse(romanToInt("MCMXCIV") == 11910)
 
-@pytest.mark.xfail
-@pytest.mark.parametrize("nums, expected", test_data_failure)
-def test_romanToInt_failure(nums, expected):
-  assert romanToInt(nums) == expected
