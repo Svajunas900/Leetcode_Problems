d343399651eb8b56bdf4372153bfda6e97fc385b
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-14 15:40:52 +0200

    Refactored test_problem_14 to use Unittest instead of pytest

--- tests/test_problem_14.py
+++ tests/test_problem_14.py
@@ -1,25 +1,15 @@
-import pytest
+import unittest
 from problem_14 import longestCommonPrefix
 
 
-test_data_success = [(["flower","flow","flight"], "fl"),
-             (["dog","racecar","car"], ""),
-             ( ["race","racecar","racoon"], "rac"),
-             ]
+class TestThreeSum(unittest.TestCase):
+  def test_success(self):
+    self.assertEqual(longestCommonPrefix(["flower", "flow", "flight"]), "fl")
+    self.assertEqual(longestCommonPrefix(["dog", "racecar", "car"]), "")
+    self.assertEqual(longestCommonPrefix(["race","racecar","racoon"]), "rac")
 
-test_data_failure = [(["flower","flow","flight"], True),
-             (["lower","flow","flight"], "fl"),
-             ( "MCMXCIV", "fl"),
-             ( 241, "True"),
-             ]
-
-
-@pytest.mark.parametrize("nums, expected", test_data_success)
-def test_longestCommonPrefix_success(nums, expected):
-  assert longestCommonPrefix(nums) == expected
-
-
-@pytest.mark.xfail
-@pytest.mark.parametrize("nums, expected", test_data_failure)
-def test_longestCommonPrefix_failure(nums, expected):
-  assert longestCommonPrefix(nums) == expected
+  def test_failure(self):
+    self.assertFalse(longestCommonPrefix(["flower", "flow", "flight"]) == [[-1, -1, 2], [-1, 0, 1]])
+    self.assertFalse(longestCommonPrefix(["race","racecar","racoon"]) == "flw")
+    self.assertFalse(longestCommonPrefix(["flower", "flow", "flight"]) == [[-60, 20, 40], [-40, 20, 20]])
+    self.assertFalse(longestCommonPrefix(["dog", "racecar", "car"]) == 12)
