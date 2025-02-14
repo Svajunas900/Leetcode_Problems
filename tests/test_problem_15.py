ca5994f85a168e955f2b7caeddd5d5115dceadab
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-14 15:27:29 +0200

    Refactored test_problem_15 to use Unittest instead of pytest

--- tests/test_problem_15.py
+++ tests/test_problem_15.py
@@ -1,28 +1,17 @@
-import pytest
+import unittest
 from problem_15 import threeSum
 
 
-test_data_success = [
-             ([-1,0,1,2,-1,-4], [[-1, -1, 2], [-1, 0, 1]]),
-             ([0,1,1, -2, -3], [[-2, 1, 1]]),
-             ([20,52,22,40,-40,20,-60], [[-60, 20, 40], [-40, 20, 20]]),
-             ([31,63,32,1,-63, 0], [[-63, 0, 63], [-63, 31, 32]]),
-             ]
+class TestThreeSum(unittest.TestCase):
+  def test_success(self):
+    self.assertEqual(threeSum([-1,0,1,2,-1,-4]), [[-1, -1, 2], [-1, 0, 1]])
+    self.assertEqual(threeSum([0,1,1, -2, -3]), [[-2, 1, 1]])
+    self.assertEqual(threeSum([20,52,22,40,-40,20,-60]), [[-60, 20, 40], [-40, 20, 20]])
+    self.assertEqual(threeSum([31,63,32,1,-63, 0]), [[-63, 0, 63], [-63, 31, 32]])
 
-test_data_failure = [
-             ([-1,0,1,2,-1,-4], []),
-             ([1,4,6,1], [3, 0]),
-             ([20,52,22,40], [2, 0]),
-             ([31,63,32,1], [2, 0]),
-             ]
+  def test_failure(self):
+    self.assertFalse(threeSum([-1,0,1,2,-1,-4]) != [[-1, -1, 2], [-1, 0, 1]])
+    self.assertFalse(threeSum([0,1,1, -2, -3]) != [[-2, 1, 1]])
+    self.assertFalse(threeSum([20,52,22,40,-40,20,-60]) != [[-60, 20, 40], [-40, 20, 20]])
+    self.assertFalse(threeSum([31,63,32,1,-63, 0]) != [[-63, 0, 63], [-63, 31, 32]])
 
-
-@pytest.mark.parametrize("nums, expected", test_data_success)
-def test_threeSum_success(nums, expected):
-  assert threeSum(nums) == expected
-
-
-@pytest.mark.xfail
-@pytest.mark.parametrize("nums, expected", test_data_failure)
-def test_threeSum_failure(nums, expected):
-  assert threeSum(nums) == expected
