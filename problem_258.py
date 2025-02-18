4433947c2bb492923d2ce8735f52f54ed56011b7
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-18 09:55:40 +0200

    Add Leetcode problem_258

---
+++ problem_258.py
@@ -0,0 +1,12 @@
+"""258. Add Digits
+
+Given an integer num, 
+repeatedly add all its digits until the result has only one digit, 
+and return it.
+"""
+
+
+def addDigits(self, num: int) -> int:
+  if num == 0:
+      return 0
+  return 1 + (num - 1) % 9
