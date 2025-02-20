4c49420fadcda96a67a1345ddc13baf3c8eca643
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 13:38:30 +0200

    Add pandas solution to problem_584

--- SQL_problems/problem_584.py
+++ SQL_problems/problem_584.py
@@ -8,4 +8,9 @@ Return the result table in any order.
 The result format is in the following example.
 """
 
-# SELECT name FROM Customer WHERE referee_id != 2 or referee_id is null;
+# SELECT name FROM Customer WHERE referee_id != 2 or referee_id is null;\
+
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+  return customer[(customer["referee_id"] != 2) | (customer["referee_id"].isna())][["name"]]
