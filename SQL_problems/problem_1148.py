caded004f16ec75360fce174f22f29d894a49537
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 10:55:14 +0200

    Add Leetcode problem_1148

---
+++ SQL_problems/problem_1148.py
@@ -0,0 +1,11 @@
+"""1148. Article Views I
+
+Write a solution to find all the authors that viewed 
+at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
+"""
+
+# SELECT DISTINCT author_id as id FROM Views WHERE author_id = viewer_id ORDER BY author_id
