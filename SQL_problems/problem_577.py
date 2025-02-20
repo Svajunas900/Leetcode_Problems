239768127823c3c90de88c53ddb70e73f8669659
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 15:02:12 +0200

    Add pandas solution to problem_577

--- SQL_problems/problem_577.py
+++ SQL_problems/problem_577.py
@@ -11,3 +11,9 @@ The result format is in the following example.
 
 # SELECT Employee.name, Bonus.bonus FROM Employee LEFT JOIN Bonus ON Bonus.empId = Employee.empId 
 # WHERE Bonus.bonus < 1000 OR Bonus.bonus is null;
+
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+  joined_df = pd.merge(employee, bonus, on="empId", how="left")
+  return joined_df[(joined_df["bonus"] < 1000) | (joined_df["bonus"].isna())][["name", "bonus"]]
