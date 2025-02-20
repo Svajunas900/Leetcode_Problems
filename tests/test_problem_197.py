42677be9132f5adc0242e7bea7742c85d4af77a8
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 16:28:02 +0200

    Create test files for each sql_problems file

---
+++ tests/test_problem_197.py
@@ -0,0 +1,12 @@
+from sql_problems.problem_197 import rising_temperature
+import pandas as pd 
+
+def test_rising_temperature():
+  pass
+
+# def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+#     weather.sort_values(by='recordDate', inplace=True)
+#     return weather[
+#         (weather.temperature.diff() > 0)
+#       & (weather.recordDate.diff().dt.days == 1)
+#     ][['id']]
