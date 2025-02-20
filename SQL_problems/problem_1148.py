16f0fb1e427aa32e45a58533b3e07cdf417289c6
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 13:51:15 +0200

    Add pandas solution to problem_1148

--- SQL_problems/problem_1148.py
+++ SQL_problems/problem_1148.py
@@ -9,3 +9,8 @@ The result format is in the following example.
 """
 
 # SELECT DISTINCT author_id as id FROM Views WHERE author_id = viewer_id ORDER BY author_id
+
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    return views[views["author_id"] == views["viewer_id"]][["author_id"]].drop_duplicates().sort_values("author_id").rename(columns={"author_id": "id"})
