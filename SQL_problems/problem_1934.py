47a8575e530d36d0a4f770aa262a995b514c3eba
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 15:31:46 +0200

    Add pandas solution to problem_1934

--- SQL_problems/problem_1934.py
+++ SQL_problems/problem_1934.py
@@ -18,3 +18,11 @@ The result format is in the following example.
 # LEFT JOIN Confirmations c 
 # ON s.user_id = c.user_id 
 # GROUP BY s.user_id
+
+import pandas as pd
+
+def confirmation_rate(signups: pd.DataFrame, confirmations: pd.DataFrame) -> pd.DataFrame:
+  confirmations['confirmation_rate'] = confirmations['action'].apply(lambda x:1 if x == 'confirmed' else 0)
+  avg_conf = confirmations[['user_id','confirmation_rate']].groupby('user_id')['confirmation_rate'].mean().round(2).reset_index()
+  output = pd.merge(signups['user_id'],avg_conf,how='left').fillna(0)  
+  return output
