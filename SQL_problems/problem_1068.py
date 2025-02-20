ed0e7a751daece2845c0035ce771204630ed91cc
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 14:08:02 +0200

    Add pandas solution to problem_1068

--- SQL_problems/problem_1068.py
+++ SQL_problems/problem_1068.py
@@ -9,5 +9,14 @@ Return the resulting table in any order.
 The result format is in the following example.
 """
 
-# SELECT Product.product_name, Sales.year, Sales.price as price FROM Sales 
-# LEFT JOIN Product ON Product.product_id = Sales.product_id;
+# SELECT Product.product_name, Sales.year, Sales.price as price 
+# FROM Sales 
+# LEFT JOIN Product 
+# ON Product.product_id = Sales.product_id;
+
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+  joined_df = pd.merge(sales, product, on="product_id", how="left")
+  return joined_df[["product_name", "year", "price"]]
+
