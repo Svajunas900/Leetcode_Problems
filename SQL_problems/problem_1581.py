59881f4f2c7055a149e1bcb138044b2780e04c37
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 14:25:15 +0200

    Add pandas solution to problem_1581

--- SQL_problems/problem_1581.py
+++ SQL_problems/problem_1581.py
@@ -9,4 +9,14 @@ The result format is in the following example.
 """
 
 # SELECT Visits.customer_id, COUNT(*) as count_no_trans FROM Visits 
-# LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id WHERE Transactions.visit_id IS NULL GROUP BY Visits.customer_id;   
+# LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id 
+# WHERE Transactions.visit_id IS NULL 
+# GROUP BY Visits.customer_id;   
+
+import pandas as pd
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+  visitors = transactions['visit_id']
+  df = visits[~(visits['visit_id'].isin(visitors))]
+  result = df.groupby('customer_id')['visit_id'].nunique().reset_index(name = 'count_no_trans')
+  return result
