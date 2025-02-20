cca75c2382e270f9c3903778c3156420ee6a7865
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 15:11:12 +0200

    Add pandas solution to problem_1280

--- SQL_problems/problem_1280.py
+++ SQL_problems/problem_1280.py
@@ -15,3 +15,13 @@ The result format is in the following example.
 # AND sb.subject_name = e.subject_name
 # GROUP BY s.student_id, e.subject_name, sb.subject_name
 # ORDER BY s.student_id, sb.subject_name
+
+import pandas as pd
+
+def students_and_examinations(students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame) -> pd.DataFrame:
+  merged_df = students.merge(subjects, how = 'cross')
+  df = examinations.groupby(['student_id','subject_name'])['subject_name'].size().reset_index(name = 'attended_exams')
+  final_df = merged_df.merge(df, how = 'left')
+  final_df.sort_values(by = ['student_id','subject_name'], ascending = True, inplace = True)
+  final_df['attended_exams'] = final_df['attended_exams'].fillna(0)
+  return final_df
