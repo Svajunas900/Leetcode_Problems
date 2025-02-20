a95cc3d69f7514934aedcac30ba87d76a7677729
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 14:41:25 +0200

    Add pandas solution to problem_197

--- SQL_problems/problem_197.py
+++ SQL_problems/problem_197.py
@@ -17,3 +17,12 @@ The result format is in the following example.
 #     WHERE current_day.temperature > yesterday.temperature
 #     AND current_day.recordDate = yesterday.recordDate + INTERVAL 1 DAY
 # );
+
+import pandas as pd
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather.sort_values(by='recordDate', inplace=True)
+    return weather[
+        (weather.temperature.diff() > 0)
+      & (weather.recordDate.diff().dt.days == 1)
+    ][['id']]
