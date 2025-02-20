ca6d5968d87c276037c5e15303aa0d94fbc1a325
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 13:58:25 +0200

    Add pandas solution to problem_1683

--- SQL_problems/problem_1683.py
+++ SQL_problems/problem_1683.py
@@ -10,3 +10,8 @@ The result format is in the following example.
 """
 
 # SELECT tweet_id FROM Tweets WHERE LENGTH(content) > 15;
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    return tweets[tweets["content"].str.len() > 15][["tweet_id"]]
