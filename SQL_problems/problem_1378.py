95126132617c3cae4563f1206515b620aa823bf4
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-20 14:04:58 +0200

    Add pandas solution to problem_1378

--- SQL_problems/problem_1378.py
+++ SQL_problems/problem_1378.py
@@ -9,4 +9,13 @@ Return the result table in any order.
 The result format is in the following example.
 """
 
-# SELECT unique_id, name FROM Employees LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id; 
+# SELECT unique_id, name 
+# FROM Employees 
+# LEFT JOIN EmployeeUNI 
+# ON Employees.id = EmployeeUNI.id; 
+
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    joined_df = pd.merge(employees, employee_uni, on="id", how="left")
+    return joined_df[["unique_id", "name"]]
