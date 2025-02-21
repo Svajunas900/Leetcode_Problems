cc4894b76d9bb024ce58b04b904dd7a710e9dec0
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-21 11:25:21 +0200

    Add Leetcode problem_550

---
+++ SQL_problems/problem_550.py
@@ -0,0 +1,17 @@
+"""550. Game Play Analysis IV
+
+
+Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, 
+rounded to 2 decimal places. 
+In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, 
+then divide that number by the total number of players.
+
+The result format is in the following example.
+"""
+
+# SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
+# FROM Activity
+# WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+# IN (SELECT player_id, MIN(event_date) AS first_login 
+#     FROM ACTIVITY 
+#     GROUP BY player_id)
