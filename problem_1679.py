875b6e2849007fbbf8abffef798249521765c8f6
Svajunas900 <svajunaskoncius11@gmail.com>
2025-03-14 08:50:21 +0200

    Add Leetcode problem_1679

---
+++ problem_1679.py
@@ -0,0 +1,26 @@
+"""1679. Max Number of K-Sum Pairs
+
+You are given an integer array nums and an integer k.
+
+In one operation, 
+you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+"""
+def maxOperations(nums: list[int], k: int) -> int:
+  nums.sort()
+
+  left = 0 
+  right = len(nums) - 1
+  operation = 0 
+
+  while left < right:
+      if ((nums[left] + nums[right]) == k):
+          operation += 1
+          left +=1 
+          right -=1
+      elif((nums[left] + nums[right]) < k):
+          left += 1
+      else:
+          right -= 1
+  return operation
