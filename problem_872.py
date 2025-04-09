d17ce2c7d1e51028c317396a7ae4b4d9ed3eb331
Svajunas900 <svajunaskoncius11@gmail.com>
2025-04-09 10:09:19 +0300

    Add Leetcode problem_872 dfs solution

---
+++ problem_872.py
@@ -0,0 +1,23 @@
+"""872. Leaf-Similar Trees
+
+Consider all the leaves of a binary tree, from left to right order, 
+the values of those leaves form a leaf value sequence.
+"""
+
+
+# DFS Solution
+def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+  leafs_1 = self.dfs(root1, [])
+  leafs_2 = self.dfs(root2, [])
+  return leafs_1 == leafs_2
+
+def dfs(self, root, leafs):
+  if not root:
+      return None
+  if root.left:
+      self.dfs(root.left, leafs)
+  if root.right:
+      self.dfs(root.right, leafs)
+  if not root.right and not root.left:
+      leafs.append(root.val)
+  return leafs
