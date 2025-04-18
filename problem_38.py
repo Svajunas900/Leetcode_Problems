3960b174eef6a7732a158986622564404f336318
Svajunas900 <svajunaskoncius11@gmail.com>
2025-04-18 09:41:59 +0300

    Add another version of problem_38

--- problem_38.py
+++ problem_38.py
@@ -14,20 +14,43 @@ replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus t
 Given a positive integer n, return the nth element of the count-and-say sequence.
 """
 
+
 def countAndSay(self, n: int) -> str:
-  def recursion(x,times):
-      if times == n:
-          return x
-      string = x[0]
-      count = 1
-      answer = ""
-      for i in range(1,len(x)):
-          if x[i] == string[0]:
-              count += 1
-          else:
-              answer += str(count)+string
-              string = x[i]
-              count = 1
-      answer += str(count)+string
-      return recursion(answer,times+1)
-  return recursion("1",1)
+    def recursion(x, times):
+        if times == n:
+            return x
+        string = x[0]
+        count = 1
+        answer = ""
+        for i in range(1, len(x)):
+            if x[i] == string[0]:
+                count += 1
+            else:
+                answer += str(count)+string
+                string = x[i]
+                count = 1
+        answer += str(count)+string
+        return recursion(answer, times+1)
+    return recursion("1", 1)
+
+#  def countAndSay(self, n: int) -> str:
+#         if n == 1:
+#             return "1"
+#         def _helper(string):
+#             prev = string[0]
+#             counter = 1
+#             result = []
+#             for i in range(1, len(string)):
+#                 curr = string[i]
+#                 if prev == curr:
+#                     counter += 1
+#                 else:
+#                     result.append(str(counter)+prev)
+#                     counter = 1
+#                 prev = curr
+#             result.append(str(counter)+prev)
+#             return "".join(result)
+#         ans = "1"
+#         for i in range(1,n):
+#             ans = _helper(ans)
+#         return ans
